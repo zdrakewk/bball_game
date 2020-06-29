8f0513177151d27af74b46f180f27bac0188b134
github username <github email address>
2020-06-28 23:22:53 -0500

    Add notes

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,3 +2,6 @@ class Game < ApplicationRecord
   belongs_to :away_team, class_name: 'Team'
   belongs_to :home_team, class_name: 'Team'
 end
+
+# good explanation article however rails 5 doesn't need 'shoulda' gem
+# article link: https://emcorrales.com/blog/rails-setup-multiple-associations-same-model
--- app/models/team.rb
+++ app/models/team.rb
@@ -10,15 +10,15 @@ Team.create(name: 'team1')
 Team.create(name: 'team2')
 
 # # create a game 
-# g1 = Game.new
+g1 = Game.new
 # # associated teams
-# g1.away_team = Team.first #makes team 1 be the away team
-# g1.home_team = Team.last
-# g1.save
+g1.away_team = Team.first #makes team 1 be the away team
+g1.home_team = Team.last
+g1.save
 # # swap associated teams
-# g2 = Game.new
-# g2.away_team = Team.last
-# g2.home_team = Team.first #makes team 1 be the home team
-# g2.save
-# Team.first.home_games # returns g2
-# Team.first.away_games # returns g1
+g2 = Game.new
+g2.away_team = Team.last
+g2.home_team = Team.first #makes team 1 be the home team
+g2.save
+Team.first.home_games # returns g2
+Team.first.away_games # returns g1
